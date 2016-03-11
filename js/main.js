7258575a08583330fc6f5fe9786b720253091f7e
Lenzo <lenbunce@Lens-MacBook-Pro.local>
2016-03-11 11:03:33 -0500

    sometimes wizards forget semicolons

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var $belowHeading = $('<h2>');
 var $aboveHeading = $('<h2>');
 var $body = $('body');
-var $li = $('li')
+var $li = $('li');
 var $belowUl = $('<ul>');
 var $aboveUl = $('<ul>');
 
@@ -12,15 +12,14 @@ $aboveHeading.html('Above ground veggies');
 $body.append($belowHeading);
 $body.append($belowUl);
 
-
 $body.append($aboveHeading);
 $body.append($aboveUl);
 
 $li.each(function () {
-	if ($(this).hasClass('below')) {
-		$belowUl.append($(this));
-} else {
-		$aboveUl.append($(this));
-}
+  if ($(this).hasClass('below')) {
+    $belowUl.append($(this));
+  } else {
+    $aboveUl.append($(this));
+  }
 
 });
